8e286bda7dfd9489fc99aad4d18bcecff175691f
michael <michael.sorsater@gmail.com>
2017-12-29 20:50:19 +0100

    Improving output format in the classifier and reorganizing of functions

--- src/bayes.py
+++ src/bayes.py
@@ -5,14 +5,23 @@ from collections import Counter
 from w8m8 import progressbar
 
 class Classy(NaiveBayesClassifier):
-    def __init__(self, corpus, genres, freq_thresh=10):
-        print('Classifier with threshold value: "{}"'.format(freq_thresh))
+    def __init__(self, corpus, genres, freq_thresh):
+        '''
+        Initializer for the class.
+        Input arguments is:
+            'corpus': A list of all songs together with their genre
+            'genres': A list of the genres to be considered
+            'freq_thresh: Integer of how many times a word need to be available in corpus to be considered
+        '''
         # List of lyrics with their correct tag (genre)
         self.corpus = corpus
+
+        self.num_songs = len(self.corpus)
 
         # List of all genres
         self.genres = genres
 
+        # The threshold for the word frequencies
         self.freq_thresh = freq_thresh
 
         # All words with their frequency
@@ -22,11 +31,14 @@ class Classy(NaiveBayesClassifier):
         self.common_words = [word for word, value in self.all_unique_words.items() if value > self.freq_thresh]
 
         self.feature_corpus = []
-        print()
 
     def get_features(self, document, n):
+        '''
+        The features extracted are defined here.
+        '''
         words_in_document = set(document)
         features = {}
+        # Occurency of a word or not in a song
         for word in self.common_words:
             features['word({})'.format(word)] = (word in words_in_document)
         return features
@@ -35,54 +47,61 @@ class Classy(NaiveBayesClassifier):
         '''
         For each song, extract all features and add to "feature_corpus"
         '''
-        print('Extracting features')
         for idx, song in enumerate(self.corpus):
-            progressbar((idx+1)/len(self.corpus))
+            progressbar((idx+1)/len(self.corpus), 'Extracting features {}/{}'.format(idx+1, self.num_songs))
             self.feature_corpus.append([
                 self.get_features(song['lyric'], song['n']),
                 #self.get_features(song['lyric']),
                 song['genre'],
             ])
         print()
-        print()
 
     def show_features(self, n):
+        '''
+        Show the n most important features
+        '''
         self.model.show_most_informative_features(n)
 
     def split_train_test(self, percent):
-        print('Splitting in train and test: {:.0f}:{:.0f}'.format(percent, (100-percent)))
         len_train = int(percent/100*len(self.feature_corpus))
         self.train_set = self.feature_corpus[:len_train]
         self.test_set = self.feature_corpus[len_train:]
 
-        print('Total songs: {}'.format(len(self.feature_corpus)))
-        print('Train: {}, test: {}'.format(len(self.train_set), len(self.test_set)))
-
     def train(self):
-        print(self.genres)
-        print('Training')
+        '''
+        Train the model by using the built in trainer.
+        '''
+        print('Training', end='\r')
         self.model = nltk.NaiveBayesClassifier.train(self.train_set)
-        print('Training done')
-        print()
+        print('Training: done')
 
     def test_all(self):
-        print('Testing')
+        '''
+        Use nltk to classify all documents in 'test_set'.
+        Replaced by 'test_documents' below.
+        '''
+        print('Testing', end='\r')
         self.accuracy = nltk.classify.accuracy(self.model, self.test_set)
-        print('Testing done')
+        print('Testing: done')
+        print('Accuracy: {:.2f}%'.format(100*self.accuracy))
         print()
-        print('Accuracy of {} songs: {:.2f}%'.format(len(self.test_set), 100*self.accuracy))
 
-    def test_documents(self, documents, verbose=False):
+    def test_documents(self, documents):
+        '''
+        Evaluate all documents and calculate accuracy for them.
+        Is used instead of built in because possible to show progress.
+        '''
         result = []
-        print('Testing manually')
         for idx, document in enumerate(documents):
-            progressbar((idx+1)/len(documents))
+            progressbar((idx+1)/len(documents), 'Testing {}/{}'.format(idx+1,len(documents)))
             res = self.model.classify(document[0])
             result.append(res==document[1])
-            if verbose:
-                print('{}: {} / {} '.format(res == document[1],document[1], res))
         print()
         self.accuracy = sum(result)/len(result)
-        true_accuracy = nltk.classify.accuracy(self.model, self.test_set)
 
-        print('Accuracy of {} songs: {:.2f}%, ({:.2f}%)'.format(len(documents), 100*self.accuracy, 100*true_accuracy))
+        # Validate the testing by using built in test
+        #true_accuracy = nltk.classify.accuracy(self.model, self.test_set)
+        #print('Accuracy: {:.2f}%, ({:.2f}%)'.format(100*self.accuracy, 100*true_accuracy))
+
+        print('Accuracy: {:.2f}%'.format(100*self.accuracy))
+        print()
--- src/classify.py
+++ src/classify.py
@@ -9,7 +9,7 @@ import nltk
 from nltk.corpus import names
 import os
 import argparse
-from naive_bayes import Classy
+from bayes import Classy
 import json
 from collections import Counter
 
@@ -31,6 +31,7 @@ def parse_args():
     parser.add_argument('--features', type=int, default=-1, help='If provided, number of informative features to show')
     parser.add_argument('--split', type=int, default=70, help='In percent, how much is training data')
     parser.add_argument('--iterations', type=int, default=1, help='Number of iterations to run model')
+    parser.add_argument('--output', action='store_false', help='Do not use multiline print')
 
     return parser.parse_args()
 
@@ -65,19 +66,48 @@ def get_lyrics_from_file(args):
 
     return genre_distribution, lyrics, failed
 
+def run_model(args, i, songs, genres):
+    '''
+    Run one iteration of the model.
+    If the argument 'output' is provided, prints more pretty.
+    '''
+    print('===== RUNNING MODEL, iteration {}/{} ====='.format(i+1, args.iterations))
+    random.shuffle(songs)
+
+    # Create the classy object
+    classy = Classy(songs, genres, args.thresh)
+
+    # For each song, extract features
+    classy.extract_features()
+
+    # Provide percent that is train, rest is test
+    classy.split_train_test(percent=args.split)
+
+    # Train the model
+    classy.train()
+
+    # Test the model
+    #classy.test_all()
+    classy.test_documents(classy.test_set)
+
+    # Show features or not
+    if args.features >= 1:
+        classy.show_features(args.features)
+
+    # Clear previous print
+    if args.output:
+        print('\033[F\033[K' * 6, end='')
+        print('Iteration: {} Accuracy: {:.2f}%'.format(i+1, 100*classy.accuracy))
+
+    return classy
+
 if __name__ == '__main__':
     try:
         args = parse_args()
-        print(args.genres)
         print('===== READ DATA =====')
-        print('Parsing file: "{}"'.format(args.file))
+        print('Using file: "{}"'.format(args.file))
+        # Read lyrics for all songs in file
         genre_distribution, songs, failed = get_lyrics_from_file(args)
-        print()
-        print("Number of songs in total:", len(songs))
-        for genre, cntr in Counter(genre_distribution).items():
-            print('\tGenre "{}" with {} songs'.format(genre, cntr))
-
-        genres = set(genre_distribution)
 
         if failed:
             print("Failed: {}".format(len(failed)))
@@ -85,49 +115,29 @@ if __name__ == '__main__':
                 for artist, song in failed:
                     print('\t', artist, song)
 
+        print()
+        print("Number of songs in total:", len(songs))
+        num_train = int(args.split/100*len(songs))
+        print('Train and test: {:.0f}:{:.0f} ({}:{})'.format(args.split, 100-args.split, num_train, len(songs)-num_train))
+        print('Genres:')
+        for genre, cntr in Counter(genre_distribution).items():
+            print('  "{}" with {} songs'.format(genre, cntr))
+
+        print('Classifier with threshold value: {}'.format(args.thresh))
+
+        genres = set(genre_distribution)
+
+        print()
         accs = []
         for i in range(args.iterations):
-            random.shuffle(songs)
-
-            print()
-            print('===== RUNNING MODEL =====')
-            print('Iteration: {}/{}'.format(i+1, args.iterations))
-            # Create the classy object
-            classy = Classy(songs, genres, args.thresh)
-
-            # For each song, extract features
-            classy.extract_features()
-
-            # Provide percent that is train, rest is test
-            classy.split_train_test(percent=args.split)
-
-            # Train the model
-            classy.train()
-
-            # Test the model
-            #classy.test_all()
-            classy.test_documents(classy.test_set)
-
-            # Show features or not
-            if args.features >= 1:
-                classy.show_features(args.features)
-
+            classy = run_model(args, i, songs, genres)
             accs.append(classy.accuracy)
-
-        print('\n\n===== RESULT =====')
-        print('File: "{}"'.format(args.file))
-        print("Number of songs in total:", len(songs))
-        print('Train and test: {:.0f}:{:.0f} ({}:{})'.format(args.split, 100-args.split, len(classy.train_set), len(classy.test_set)))
-        print()
-        for genre, cntr in Counter(genre_distribution).items():
-            print('  Genre "{}" with {} songs'.format(genre, cntr))
-        print()
-        for acc in accs:
-            print('  Accuracy of {} songs: {:.2f}%'.format(len(classy.test_set), 100*acc))
+            prev_acc = classy.accuracy
 
         print()
+        print('===== RESULT =====')
+        print('Average accuracy: {:.2f}%'.format(100*sum(accs)/len(accs)))
 
-        print('Average accuracy of {} songs: {:.2f}%'.format(len(classy.test_set), 100*sum(accs)/len(accs)))
     except KeyboardInterrupt:
         print()
         print()
