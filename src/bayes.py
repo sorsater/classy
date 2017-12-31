e6dae6ceb0e90c7c9ce68152451dad4ce0ad0813
michael <michael.sorsater@gmail.com>
2017-12-31 19:13:11 +0100

    Adding functionality to turn features on/off. Finishing bigrams :sunglasses:

--- src/bayes.py
+++ src/bayes.py
@@ -9,35 +9,31 @@ from collections import Counter
 from w8m8 import progressbar
 
 class Classy(NaiveBayesClassifier):
-    def __init__(self, corpus, genres, args):
+    def __init__(self, corpus, args):
         '''
         Initializer for the class.
         Input arguments is:
             'corpus': A list of all songs together with their genre. Shuffled.
-            'genres': A list of the genres to be considered
             'args: Arguments from the argumentparser.
         '''
         # List of lyrics with their correct genre
         self.corpus = corpus
 
-        # List of all genres
-        self.genres = genres
+        # Features used in the model
+        self.features = args.features
 
-        # The threshold for the word frequencies
-        self.freq_thresh_uni = args.thresh
-        self.freq_thresh_bi = args.thresh
+        # The threshold for the gram frequencies
+        self.freq_thresh_uni = args.uni_thresh
+        self.freq_thresh_bi = args.bi_thresh
 
         # Percent to be train, rest is test
         self.percent = args.split / 100
 
-    def get_features_for_song(self, song):
-        '''
-        The features extracted are defined here.
-        '''
-        meta_data = []
+    def features_grams(self, lyrics):
         unigrams = set()
         bigrams = set()
-        for line in song['lyrics'].split('\n'):
+        meta_data = []
+        for line in lyrics.split('\n'):
             line = line.rstrip()
             if len(line) <= 1:
                 continue
@@ -52,16 +48,10 @@ class Classy(NaiveBayesClassifier):
                     unigrams.add(word)
                     if idx > 0:
                         bigrams.add(words[idx - 1] + word)
-        features = {}
-        # Occurency of a unigram or not in a song
-        for word in self.common_unigrams:
-            features['uni({})'.format(word)] = (word in unigrams)
 
-        # Occurency of a bigram or not in a song
-        #for word in self.common_bigrams:
-        #    features['bi({})'.format(word)] = (word in bigrams)
+        return unigrams, bigrams, meta_data
 
-        # Types of metadata in lyrics. Count all of them to keep tack of how song is structured.
+    def features_meta(self, meta_data):
         meta_members = {
             'verse': 0, 'chorus': 0, 'intro': 0, 'outro': 0,
             'hook': 0, 'bridge': 0, 'interlude': 0, 'break': 0, 'breakdown': 0,
@@ -73,10 +63,29 @@ class Classy(NaiveBayesClassifier):
             for key in meta_members.keys():
                 if key in item:
                     meta_members[key] += 1
+        return meta_members
 
-        for item, cnt in meta_members.items():
-            #features[item] = cnt
-            pass
+    def get_features_for_song(self, song):
+        '''
+        The features extracted are defined here.
+        '''
+
+        unigrams, bigrams, meta_data = self.features_grams(song['lyrics'])
+
+        features = {}
+        # Occurency of a unigram or not in a song
+        for unigram in self.common_unigrams:
+            features['uni({})'.format(unigram)] = (unigram in unigrams)
+
+        # Occurency of a bigram or not in a song
+        if 'bigram' in self.features:
+            for bigram in self.common_bigrams:
+                features['bi({})'.format(bigram)] = (bigram in bigrams)
+
+        # Types of metadata in lyrics. Count all of them to keep tack of how song is structured.
+        if 'meta' in self.features:
+            for item, cnt in self.features_meta(meta_data).items():
+                features[item] = cnt
 
         return features
 
@@ -85,14 +94,16 @@ class Classy(NaiveBayesClassifier):
         For each song in the dataset, extract all features.
         '''
         features = []
+        names = []
         for idx, song in enumerate(data_set):
-            progressbar((idx+1)/len(data_set), 'Features "{}" {}/{}'.format(name, idx+1, len(data_set)))
+            progressbar((idx+1)/len(data_set), 'Features {} {}/{}'.format(name, idx+1, len(data_set)))
             features.append([
                 self.get_features_for_song(song),
                 song['genre'],
             ])
+            names.append(song['name'])
         print()
-        return features
+        return names, features
 
     def split_train_test(self):
         len_train = int(self.percent * len(self.corpus))
@@ -103,7 +114,7 @@ class Classy(NaiveBayesClassifier):
         # Avoid to look at test data (no cheating)
         unigrams = []
         bigrams = []
-        for song in self.train_raw:
+        for idx, song in enumerate(self.train_raw):
             for line in song['lyrics'].split('\n'):
                 if len(line) <= 1:
                     continue
@@ -122,16 +133,16 @@ class Classy(NaiveBayesClassifier):
                         if idx > 0:
                             bigrams.append(words[idx - 1] + word)
 
-        self.unique_unigrams = Counter(unigrams)
-        self.unique_bigrams = Counter(bigrams)
+        unique_unigrams = Counter(unigrams)
+        unique_bigrams = Counter(bigrams)
 
         # Words with a higher frequency than 'freq_thresh' is stored in 'common_unigrams'
-        self.common_unigrams = set([word for word, value in self.unique_unigrams.items() if value > self.freq_thresh_uni])
-        self.common_bigrams = set([word for word, value in self.unique_bigrams.items() if value > 5])#self.freq_thresh_bi])
+        self.common_unigrams = set([word for word, value in unique_unigrams.items() if value >= self.freq_thresh_uni])
+        self.common_bigrams = set([word for word, value in unique_bigrams.items() if value >= self.freq_thresh_bi])
 
         # Create train and test set
-        self.train_set = self.extract_features(self.train_raw, 'train')
-        self.test_set = self.extract_features(self.test_raw, 'test')
+        self.train_names, self.train_set = self.extract_features(self.train_raw, 'train')
+        self.test_names, self.test_set = self.extract_features(self.test_raw, 'test')
 
     def show_features(self, n):
         '''
@@ -151,12 +162,13 @@ class Classy(NaiveBayesClassifier):
         # You probably need to uncomment the print if you don't do the same
         # print('Training: done')
 
-    def test_all(self):
+    def test_old(self):
         '''
         Use nltk to classify all documents in 'test_set'.
         Replaced by 'test' below.
         '''
         self.accuracy = nltk.classify.accuracy(self.model, self.test_set)
+        print('Testing')
         print('Accuracy: {:.2f}%'.format(100*self.accuracy))
         print()
 
@@ -165,17 +177,18 @@ class Classy(NaiveBayesClassifier):
         Evaluate all documents and calculate accuracy for them.
         Is used instead of built in because possible to show progress.
         '''
+        # List with 'True'/'False' if correctly guessed or not
         result = []
-        for idx, document in enumerate(self.test_set):
-            progressbar((idx+1)/len(self.test_set), 'Testing {}/{}'.format(idx+1,len(self.test_set)))
-            res = self.model.classify(document[0])
-            result.append(res==document[1])
-        print()
-        self.accuracy = sum(result)/len(result)
+        for idx, (lyrics_features, true_genre) in enumerate(self.test_set):
+            progressbar((idx+1)/len(self.test_set), 'Testing {}/{}'.format(idx+1, len(self.test_set)))
 
-        # Validate the testing by using built in "test"
-        #true_accuracy = nltk.classify.accuracy(self.model, self.test_set)
-        #print('Accuracy: {:.2f}%, ({:.2f}%)'.format(100*self.accuracy, 100*true_accuracy))
+            result.append(true_genre == self.model.classify(lyrics_features))
+
+            # Name of current test song
+            #self.test_names[idx]
+
+        print()
+        self.accuracy = sum(result) / len(result)
 
         print('Accuracy: {:.2f}%'.format(100*self.accuracy))
         print()
--- src/classify.py
+++ src/classify.py
@@ -11,78 +11,88 @@ import os
 import argparse
 from bayes import Classy
 import json
-from collections import Counter
+from collections import Counter, OrderedDict
+from time import time
 
-random.seed(12345)
+# Available features.
+# The first element should be all, the second is the default
+all_features = OrderedDict([
+    ('all', 'Add all features to the baseline'),
+    ('baseline', 'The baseline system'),
+    ('bigram', 'Bigrams in the songs'),
+    ('meta', 'Song structure, use data about "Verse", "Chorus" etc'),
+    ('stopwords', 'Remove stopwords'),
+    ('stem', 'Stem all words'),
+])
 
-def parse_args():
+def parse_args(arguments=[]):
     '''
     Parse arguments.
     Only required argument is .json file with artist, song, genres
     '''
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument('file', type=str, help='Name of json file')
-    parser.add_argument('--genres', nargs='*', choices=['all', 'baseline', 'pop', 'rap', 'rock', 'country', 'electronic', 'rob'], default=['all'], help='Genres to be parsed')
-    parser.add_argument('--iterations', type=int, default=1, help='Number of iterations to run model')
-    parser.add_argument('--thresh', type=int, default=10, help='Number of occurences for a word to be in model')
-    parser.add_argument('--split', type=int, default=70, help='In percent, how much is training data')
-
-    parser.add_argument('--features', type=str, nargs='*', default=['all'], help='Features to be used, default none.')
+    parser.add_argument('-g', '--genres', nargs='*', default=['all'], help='Genres to be parsed',
+        choices=['all', 'baseline', 'pop', 'rap', 'rock', 'country', 'electronic', 'rob'])
+    parser.add_argument('-i', '--iterations', type=int, default=1, help='Number of iterations to run model')
+    parser.add_argument('-u', '--uni_thresh', type=int, default=10, help='Number of occurences for a unigram to be in model')
+    parser.add_argument('-b', '--bi_thresh', type=int, default=10, help='Number of occurences for a bigram to be in model')
+    parser.add_argument('-s', '--split', type=int, default=70, help='In percent, how much is training data')
+    parser.add_argument('-f', '--features', type=str, nargs='*', default=[], help='Features to be used, default none.',
+        choices=list(all_features.keys()))
 
     parser.add_argument('--output', action='store_false', help='Do not use multiline print')
     parser.add_argument('--folder_name', type=str, default='lyrics', help='Name of folder to look for songs')
     parser.add_argument('--failed', action='store_true', help='Show the failed songs')
     parser.add_argument('--show', type=int, default=-1, help='If provided, number of informative features to show')
 
-    return parser.parse_args()
+    return parser.parse_args(arguments) if arguments else parser.parse_args()
 
 def get_lyrics_from_file(args):
     '''
     From specified "args.file", return lyrics from "args.folder_name"
     '''
 
-    lyrics = []
-    with open(args.file) as f:
-        data = json.load(f)
-
-    failed = []
+    corpus = []
     genre_distribution = []
-    for idx, entry in data.items():
+    failed = []
+    for idx, entry in json.load(open(args.file)).items():
         artist, song, genre = entry
         song_path = os.path.join(args.folder_name, artist.replace('/','') + '~' + song.replace('/',''))
+
+        if ('all' not in args.genres) and (genre not in args.genres):
+            continue
 
         if not os.path.exists(song_path):
             failed.append([artist, song])
             continue
 
-        if args.genres != ['all'] and genre not in args.genres:
-            continue
-
         genre_distribution.append(genre)
 
-        lyrics.append({
-            'name': artist + song,
+        corpus.append({
+            'name': artist + '~' + song,
             'lyrics': open(song_path).read(),
             'genre': genre,
         })
 
-    return genre_distribution, lyrics, failed
+    return genre_distribution, corpus, failed
 
-def run_model(args, i, songs, genres):
+def run_model(i, corpus, args):
     '''
     Run one iteration of the model.
     If the argument 'output' is provided, prints more pretty.
     The inte
     '''
-    # Might need to be modified so print appers as expected
+    # Might need to be modified so print behaves as expected
     printed_lines = 7
 
-    print('===== RUNNING MODEL, iteration {}/{} ====='.format(i+1, args.iterations))
-    random.shuffle(songs)
+    t_run = time()
 
-    classy = Classy(songs, genres, args)
+    print('===== RUNNING MODEL, iteration {}/{} ====='.format(i+1, args.iterations))
+    random.shuffle(corpus)
+
+    classy = Classy(corpus, args)
 
     classy.split_train_test()
 
@@ -100,46 +110,73 @@ def run_model(args, i, songs, genres):
     # Clear previous print
     if args.output:
         print('\033[F\033[K' * printed_lines, end='')
-        print('Iteration: {} Accuracy: {:.2f}%'.format(i+1, 100*classy.accuracy))
+        print('{}: Accuracy: {:.2f}%, Time: {:.1f} seconds'.format(i+1, 100*classy.accuracy, time() - t_run))
 
-    return classy
+    return classy.accuracy
 
-if __name__ == '__main__':
+def main(args):
     try:
-        args = parse_args()
-        print('===== READ DATA =====')
+        # Reset the seed after each test
+        random.seed(12345)
+
+        t_start = time()
+        print()
+        print('===== READING DATA =====')
         print('Using file: "{}"'.format(args.file))
         # Read lyrics for all songs in file
-        genre_distribution, songs, failed = get_lyrics_from_file(args)
+        genre_distribution, corpus, failed = get_lyrics_from_file(args)
+        # Creates a dict of the genres and their song count
+        args.genres = Counter(genre_distribution)
 
         if failed:
-            print("Failed: {}".format(len(failed)))
-            if args.failed:
+            print('Failed: {}'.format(len(failed)))
+            # Toggle to see songs that failed
+            if False:
                 for artist, song in failed:
-                    print('\t', artist, song)
+                    print(' "{}": "{}"'.format(artist, song))
+        print()
+        print('Number of songs in total: {}'.format(len(corpus)))
+        for genre, cntr in args.genres.items():
+            print('  {}: "{}" '.format(cntr, genre))
+        num_train = int(args.split/100*len(corpus))
 
         print()
-        print("Number of songs in total:", len(songs))
-        for genre, cntr in Counter(genre_distribution).items():
-            print('  {}: "{}" '.format(cntr, genre))
-        num_train = int(args.split/100*len(songs))
-        print('Train and test: {:.0f}:{:.0f} ({}:{})'.format(args.split,100-args.split, num_train, len(songs)-num_train))
+        print('===== MODEL PARAMETERS =====')
+        print('Train and test: {:.0f}:{:.0f} ({}:{})'.format(args.split,100-args.split, num_train, len(corpus)-num_train))
+
+        if 'all' in args.features:
+            args.features = list(all_features.keys())[1:]
+        else:
+            # Add baseline
+            args.features = [list(all_features.keys())[1]] + args.features
+
         print()
-        print('Classifier with freq value: {}'.format(args.thresh))
+        print('Features:')
+        for feature, description in all_features.items():
+            if feature in args.features:
+                print('  {}: \t {}'.format(feature, all_features[feature]))
+
+        print()
+        print('Classifier with unigram value: {}'.format(args.uni_thresh))
+        if 'bigram' in args.features:
+            print('Classifier with bigram value: {}'.format(args.bi_thresh))
         print('Number of iterations: {}'.format(args.iterations))
 
-        genres = set(genre_distribution)
-
+        # Run model "args.iterations" times
         print()
-        accs = []
-        for i in range(args.iterations):
-            classy = run_model(args, i, songs, genres)
-            accs.append(classy.accuracy)
-            prev_acc = classy.accuracy
+        accs = [run_model(i, corpus, args) for i in range(args.iterations)]
+
+        total_accuracy = sum(accs) / len(accs)
+        total_time = time() - t_start
 
         print()
         print('===== RESULT =====')
-        print('Average accuracy: {:.2f}%'.format(100*sum(accs)/len(accs)))
+        print('Average accuracy: {:.2f}%'.format(100*total_accuracy))
+
+        print()
+        print('Total time: {:.1f} seconds'.format(total_time))
+
+        return total_accuracy, total_time
 
     except KeyboardInterrupt:
         print()
@@ -147,3 +184,6 @@ if __name__ == '__main__':
         print('CANCELED')
     except Exception as e:
         print('Error: {}'.format(e))
+
+if __name__ == '__main__':
+    main(parse_args())
