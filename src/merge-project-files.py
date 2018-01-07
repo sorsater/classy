19b529e44bb344ab42224aec4cd75281951152bc
michael <michael.sorsater@gmail.com>
2018-01-07 10:59:16 +0100

    Fixing bug that not all entries were added to db-file

--- src/merge-project-files.py
+++ src/merge-project-files.py
@@ -17,6 +17,7 @@ def parse_args():
 
     parser.add_argument('files', nargs='*', type=str, help='Name of json file')
     parser.add_argument('output_file', type=str, help='Name of output file')
+    parser.add_argument('--db_file', type=str, default='url-db.json', help='List of all songs with their url')
 
     return parser.parse_args()
 
@@ -24,11 +25,13 @@ def merge_files(args):
     '''
     Takes several project files and merges them to one big
     '''
-    for potatis in args.files:
-        print(potatis)
+    for file_name in args.files:
+        print(file_name)
     print()
     print(args.output_file)
 
+    url_data_db = json.load(open(args.db_file))
+    init_len_url_data_db = len(url_data_db)
     print()
     print('Analyzing files: "{}"'.format(args.files))
     print()
@@ -39,6 +42,9 @@ def merge_files(args):
         data = json.load(open(project))
         print('Number of songs for "{}": {}'.format(project, len(data)))
         for idx, (artist, song, genre) in data.items():
+            key = str((artist, song))
+            if not key in url_data_db:
+                url_data_db[key] = ''
             new_data[cntr] = [artist, song, genre]
             cntr += 1
     print()
@@ -46,6 +52,11 @@ def merge_files(args):
     json.dump(new_data, open(args.output_file, 'w'), indent=4, sort_keys=True)
     print('Dumped in file: "{}"'.format(args.output_file))
     print()
+
+    print('Number of db_entrys before: {}'.format(init_len_url_data_db))
+    print('Number of db_entrys: {}'.format(len(url_data_db)))
+
+    json.dump(url_data_db, open(args.db_file, 'w'), indent=4)#, sort_keys=True)
 
     clean_duplicates(args.output_file)
 
